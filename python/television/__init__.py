237eac974332b0f7b8ae48a6e9a850554df4cf30
Patrick Paul <patrick@astrohaus.com>
2018-02-22 16:47:37 -0500

    adds RuntimeError for python < 3.6

--- python/television/__init__.py
+++ python/television/__init__.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
+import sys
+
+__version__ = "0.1.4"
+
+if sys.version_info < (3, 6):
+    raise RuntimeError("django-television requires Python 3.6 or later.")
+
 default_app_config = 'television.app.AppConfig'
 
 from .decorators import add_listener, call_listener, require_auth, require_staff, require_superuser, add_data_binding_staff, add_data_binding_superuser, add_data_binding_owner
